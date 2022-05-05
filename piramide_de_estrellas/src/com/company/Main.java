0b472b9228c07fd9463e932ad8641a03626939ca
wade079 <99978276+wade079@users.noreply.github.com>
2022-05-06 00:54:22 +0200

    1
    1

---
+++ piramide_de_estrellas/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("piramide de estrellas");
+        System.out.println("poner valor numerico");
+
+        Scanner valor = new Scanner(System.in);
+        int numero = valor.nextInt();
+
+        for (int altura = 0; altura <= numero; altura++) {
+
+            for (int blancos = 0; blancos <= numero - altura; blancos++) {
+                System.out.print(" ");
+
+            }
+
+            for (int asterisco = 0; asterisco <= (altura * 2) - 2; asterisco++) {
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+            valor.close();
+
+        }
+
+    }
+    
+}
